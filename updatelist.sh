52090871528224b429cc0f0209b8219eb692ba31
Zhang Huanjie <james@ustc.edu.cn>
2017-09-20 14:56:36 +0800

    HP Priter

--- updatelist.sh
+++ updatelist.sh
@@ -2,15 +2,25 @@
 
 cd /usr/src/blackhole
 
-> ip.txt
-
-curl http://222.195.81.240/scan/`date +%F`.sql.txt | grep -e "202.38.64" -e "210.45.64" -e "202.38.95" -e "222.195.70" -e "202.38.74" -e "202.38.93" >> ip.txt
-curl http://222.195.81.240/scan/`date +%F`.3389.txt | grep -e "202.38.64" -e "210.45.64"  >> ip.txt
-curl http://222.195.81.240/scan/`date +%F`.hpprinter.txt >> ip.txt
-
+curl http://222.195.81.240/scan/`date +%F`.sql.txt | grep -e "202.38.64" -e "210.45.64" -e "202.38.95" -e "222.195.70" -e "202.38.74" -e "202.38.93" > ip.txt
 cut -f3,4 -d' ' ip.txt | while read port ip; do 
 	echo $ip $port
-	./addblackhole $ip 1 tcp $port "masscan"
+	./addblackhole $ip 4 tcp $port "masscan"
+done 
+
+curl http://222.195.81.240/scan/`date +%F`.3389.txt | grep -e "202.38.64" -e "210.45.64" > ip.txt
+cut -f3,4 -d' ' ip.txt | while read port ip; do 
+	echo $ip $port
+	./addblackhole $ip 4 tcp $port "masscan"
+done 
+
+curl http://222.195.81.240/scan/`date +%F`.hpprinter.txt > ip.txt
+cut -f3,4 -d' ' ip.txt | while read port ip; do 
+	echo HP printer $ip $port
+	nc -z -w2 -v $ip 80 > /dev/null
+	if [ $? == 0 ] ; then
+		./addblackhole $ip 4 all 0 "masscan/HP Printer"
+	fi
 done 
 
 ./changeiptables.sh
